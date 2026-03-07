7e0a45b1229b482ce820b1b1c4545ede4916c875
SyntaxAdil <abdurrahmanadil005@gmail.com>
2026-03-07 06:05:31 +0600

    Implement login page and functionality

---
+++ js/login.js
@@ -0,0 +1,29 @@
+const demoCredentials = {
+  username: "admin",
+  password: "admin123",
+};
+const userNameInput = document.getElementById("user-name-input");
+const userPassInput = document.getElementById("user-pass-input");
+const userSignInBtn = document.getElementById("user-signin-btn");
+
+function handleLogin() {
+  const nameVal = userNameInput.value.trim();
+  const passVal = userPassInput.value.trim();
+
+  if (!nameVal || !passVal) {
+    alert("Fill all the fields to procced");
+    return;
+  }
+  if (
+    nameVal === demoCredentials.username &&
+    passVal === demoCredentials.password
+  ) {
+    alert("login successfull");
+    // window.location.assign(("../app.html"))
+    window.location.replace(("../app.html"))
+  } else {
+    alert("login failed");
+  }
+}
+
+userSignInBtn.addEventListener("click", handleLogin);
